d792f6dbe89b212ce159534ce2b1a7ed9043be5d
Tomasz Rajzer <trajzer@wsiz.edu.pl>
2023-10-25 13:20:05 +0200

    init

---
+++ LAB2/Constructions/Construction.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB2.Constructions
+{
+    class Construction
+    {
+        public Construction(float height, float width, int entrances,
+            int humanCapacity, string buildMaterial)
+        {
+            Height = height;
+            Width = width;
+            Entrances = entrances;
+            HumanCapacity = humanCapacity;
+            _buildMaterial = buildMaterial;
+        }
+
+        private string _buildMaterial { get; set; }
+        public float Height { get; set; }
+        public float Width { get; set; }
+        public int Entrances { get; set; }
+        public int HumanCapacity { get; set; }
+        public string BuildMaterial
+        {
+            get
+            {
+                return _buildMaterial;
+            }
+            set {
+                _buildMaterial = value;
+            }
+        }
+    }
+}
---
+++ LAB2/Program.cs
@@ -0,0 +1,15 @@
+﻿using LAB2.Constructions;
+using System;
+
+namespace LAB2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var constr = new Construction(32, 23, 3, 6, "brik");
+            constr.BuildMaterial = "eee";
+            Console.WriteLine(constr.BuildMaterial);
+        }
+    }
+}
