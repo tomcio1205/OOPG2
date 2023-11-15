ee873a62651f3163f0f28edc9cf7bb5aee94e881
Tomasz Rajzer <trajzer@wsiz.edu.pl>
2023-11-15 13:02:06 +0100

    add inheritance

---
+++ LAB2/IndustrialHalls/IndustrialHall.cs
@@ -0,0 +1,18 @@
+﻿using LAB2.Constructions;
+using LAB2.Constructions.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB2.IndustrialHalls
+{
+    class IndustrialHall: Construction
+    {
+        public IndustrialHall(): base(12, 23, 34, 31, TypeOfMaterial.Concrete)
+        {
+            
+        }
+    }
+}
--- LAB2/Program.cs
+++ LAB2/Program.cs
@@ -1,5 +1,8 @@
-﻿using LAB2.Constructions;
+﻿using LAB2.BuildingsWrapper;
+using LAB2.Constructions;
 using LAB2.Constructions.Models;
+using LAB2.IndustrialHalls;
+using LAB2.ResidentalBuildings;
 using System;
 
 namespace LAB2
@@ -20,12 +23,29 @@ namespace LAB2
             };
                 
             var constr3 = new Construction(inputParam);
-            Console.WriteLine(constr.BuildMaterial);
-            Console.WriteLine(constr2.BuildMaterial);
-            Console.WriteLine(constr2.Width);
-            Console.WriteLine(constr2.Height);
-            Console.WriteLine(constr3.BuildMaterial);
-            Console.WriteLine(constr3.GetSquareCost());
+            //Console.WriteLine(constr.BuildMaterial);
+            //Console.WriteLine(constr2.BuildMaterial);
+            //Console.WriteLine(constr2.Width);
+            //Console.WriteLine(constr2.Height);
+            //Console.WriteLine(constr3.BuildMaterial);
+            //Console.WriteLine(constr3.GetSquareCost());
+
+            var building = new ResidentalBuilding(2);
+            var hall = new IndustrialHall();
+
+            Console.WriteLine(building.BuildMaterial);
+            Console.WriteLine(building.Width);
+            Console.WriteLine(building.Height);
+
+            Console.WriteLine(hall.BuildMaterial);
+            Console.WriteLine(hall.Width);
+            Console.WriteLine(hall.Height);
+
+            var wrapper1 = new BuildingWrapper(building);
+            var wrapper2 = new BuildingWrapper(hall);
+
+            Console.WriteLine(wrapper1.GetWrappedSquareCost());
+            Console.WriteLine(wrapper2.GetWrappedSquareCost());
         }
     }
 }
